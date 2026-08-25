67c65cf2429a40bb775d0959b5d4a313a38c97fe
LAlvarez8 <leontoluco06@gmail.com>
2026-08-24 22:39:19 -0600

    feat: agregar LWC plantPhotoUploader y Apex PlantPhotoController

--- force-app/main/default/lwc/speciesWithPlants/speciesWithPlants.js
+++ force-app/main/default/lwc/speciesWithPlants/speciesWithPlants.js
@@ -1,26 +1,26 @@
-import { LightningElement, wire } from 'lwc';
-import getSpeciesWithPlants from '@salesforce/apex/SpeciesService.getSpeciesWithPlants';
+import { LightningElement, wire } from "lwc";
+import getSpeciesWithPlants from "@salesforce/apex/SpeciesService.getSpeciesWithPlants";
 
 export default class SpeciesWithPlants extends LightningElement {
-    @wire(getSpeciesWithPlants) speciesWithPlants;
+  @wire(getSpeciesWithPlants) speciesWithPlants;
 
-    get loading() {
-        return this.speciesWithPlants?.loading;
-    }
+  get loading() {
+    return this.speciesWithPlants?.loading;
+  }
 
-    get error() {
-        return this.speciesWithPlants?.error;
-    }
+  get error() {
+    return this.speciesWithPlants?.error;
+  }
 
-    get speciesList() {
-        if (this.speciesWithPlants?.data) {
-            return this.speciesWithPlants.data.map(item => {
-                return {
-                    ...item.species,
-                    Plants__r: item.plants
-                };
-            });
-        }
-        return [];
+  get speciesList() {
+    if (this.speciesWithPlants?.data) {
+      return this.speciesWithPlants.data.map((item) => {
+        return {
+          ...item.species,
+          Plants__r: item.plants
+        };
+      });
     }
+    return [];
+  }
 }
